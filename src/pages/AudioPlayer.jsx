7daa1885c9275af3c1e601d13a222c489d692cb8
Naveensnk26 <saibharath2005@gmail.com>
2025-03-27 20:21:57 +0530

    Initial commit

--- src/pages/AudioPlayer.jsx
+++ src/pages/AudioPlayer.jsx
@@ -7,7 +7,6 @@ const AudioPlayer = () => {
   const [progress, setProgress] = useState(0);
   const [volume, setVolume] = useState(1);
 
-
   const togglePlayPause = () => {
     if (isPlaying) {
       audioRef.current.pause();
@@ -16,27 +15,20 @@ const AudioPlayer = () => {
     }
     setIsPlaying(!isPlaying);
   };
-
-
   const updateProgress = () => {
     const { currentTime, duration } = audioRef.current;
     setProgress((currentTime / duration) * 100);
   };
-
-
   const changeProgress = (e) => {
     const newTime = (e.nativeEvent.offsetX / progressRef.current.offsetWidth) * audioRef.current.duration;
     audioRef.current.currentTime = newTime;
     setProgress((newTime / audioRef.current.duration) * 100);
   };
-
-
   const changeVolume = (e) => {
     const newVolume = e.target.value;
     audioRef.current.volume = newVolume;
     setVolume(newVolume);
   };
-
   return (
     <div className="flex flex-col items-center bg-rose-600 text-white p-6 rounded-lg shadow-lg w-80 mx-auto mt-10">
       <h2 className="text-xl font-semibold mb-4">React Audio Player</h2>
@@ -52,7 +44,7 @@ const AudioPlayer = () => {
           onClick={togglePlayPause}
           className="p-3 bg-blue-600 hover:bg-blue-700 rounded-full"
         >
-          {isPlaying ? "⏸️" : "▶️"}
+          {isPlaying ? "pause" : "play"}
         </button>
 
         <input
@@ -65,8 +57,6 @@ const AudioPlayer = () => {
           className="w-20"
         />
       </div>
-
-     
       <div
         ref={progressRef}
         className="w-full bg-gray-600 h-2 rounded cursor-pointer"
